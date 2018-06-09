fbbc846b1d9d81016bc2825fb44db15247bda8fb
Forrest Venable <forrest.venable@gmail.com>
2018-06-09 09:21:40 -0600

    change routes to not be hardcoded

--- website/website/urls.py
+++ website/website/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from portfolio import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
-    url(r'^about/', views.bio, name='bio'),
+    url(r'^portfolio/', include('portfolio.urls')),
 ]
