6c285b808fb6dad4d9609b02b953af22f40fc81b
Forrest Venable <forrest.venable@gmail.com>
2018-06-09 08:58:44 -0600

    add stub for index and about me page

--- website/portfolio/views.py
+++ website/portfolio/views.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("This is the portfolio index")
+
+def bio(request):
+	return HttpResponse("This is the bio page")
